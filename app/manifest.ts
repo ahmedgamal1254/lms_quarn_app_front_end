865ebca11890f9b0985a4acd6dd640c9581bc050
Ahmed Gamal <engahmedgamal086@gmail.com>
2026-01-01 16:09:01 +0200

    pwa app init 1

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from 'next'
  
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'أمكانية التمييز',
+    name: 'اكاديمية التمييز',
     short_name: 'التمييز',
     description: 'اكاديمية التمييز - نظام ادارة المنصة التعليمية',
     start_url: '/',
